26c7fb3eda581f8b0476ae40c4f97ece88e124c0
Christopher <thetoph@outlook.com>
2022-11-17 22:43:15 -0800

    personalizing about section

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -69,8 +69,18 @@ const Timeline = () => {
       <br />
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        Creative individual with the discipline to build things that will help
-        people. Passionate about music and nature &#127925; &#127794;
+        I consider myself a creative person and thats why I like coding, because
+        anything that I have an idea for I can create it. I started coding after
+        I had shoulder surgery when I was 18, I couldn't do much else. Coding
+        was the perfect hobby for sitting on my 🍑.
+      </SectionText>
+      <SectionText>
+        Why do I code now? My aim is to make a positive impact in peoples
+        lives...building software that adds a bit of joy to life rather than
+        robbing you of it. Outside of coding, I am passionate about music. I'm a
+        huge fan of chillstep, its timeless. I listen to loads of comedy
+        podcasts, Joey diaz is TREMENDOUS! Also, spending time in nature with my
+        aussie is a beautiful thing &#127925; &#127794;
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
