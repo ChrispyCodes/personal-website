dbb8a56d22bfca2be2f13c20d41f984adeaa0ee6
Christopher <thetoph@outlook.com>
2023-04-18 14:26:43 -0700

    update hero section text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,8 @@ const Hero = () => (
         Hey, <br /> I'm <SpanText>Chris</SpanText> &#9996;
       </SectionTitle>
       <SectionText>
-        A Software Engineer that likes to build things with modern tools.
+        Passionate developer building scalable software solutions for tomorrow's
+        challenges
       </SectionText>
       {/* <Button target="_blank" onClick={() => (window.location = "/Resume.pdf")}>
         Resume
